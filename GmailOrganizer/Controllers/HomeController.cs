89f7ce2aebbd431708b0446238b1da626870e525
bhanu chandra <bhanuchandra2010@live.com>
2023-03-04 00:16:11 -0600

    Add project files.

---
+++ GmailOrganizer/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using GmailOrganizer.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GmailOrganizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private GmailService _gmailService;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            string[] Scopes = { GmailService.Scope.MailGoogleCom };
+            UserCredential credential;
+            using (FileStream stream = new(
+                "./client_secret.json",
+                FileMode.Open,
+                FileAccess.Read
+            ))
+            {
+                string credPath = "token_Send";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                             GoogleClientSecrets.FromStream(stream).Secrets,
+                              Scopes,
+                              "user",
+                              CancellationToken.None,
+                              new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            // Create Gmail API service.
+            _gmailService = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "mycodebit",
+            });
+
+            ListMessagesResponse list = await _gmailService.Users.Messages.List("me").ExecuteAsync();
+
+            return Json(list);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GmailOrganizer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GmailOrganizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
